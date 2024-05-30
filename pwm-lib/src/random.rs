0f507d4e7824aa77cca239bc149258ea391af004
Ryan <rmw071102@gmail.com>
2024-05-30 19:23:51 -0400

    don't exclude similar characters

--- pwm-lib/src/random.rs
+++ pwm-lib/src/random.rs
@@ -8,7 +8,7 @@ pub fn random_password(length: usize) -> Result<String, &'static str> {
         uppercase_letters: true,
         symbols: true,
         spaces: false,
-        exclude_similar_characters: true,
+        exclude_similar_characters: false,
         strict: true,
     };
 
