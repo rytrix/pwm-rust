f5d0a39fb66971161ef262b1c20790ce88c0eb02
Ryan <rmw071102@gmail.com>
2024-07-22 15:45:10 -0400

    updated versions

---
+++ update-versions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# $1 first version
+# $2 second version
+function update() {
+    sed -i "s/version = \"$1\"/version = \"$2\"/g" pwm-gui/Cargo.toml
+    sed -i "s/version = \"$1\"/version = \"$2\"/g" pwm-cli/Cargo.toml
+    sed -i "s/version = \"$1\"/version = \"$2\"/g" pwm-lib/Cargo.toml
+    sed -i "s/version = \"$1\"/version = \"$2\"/g" pwm-proc/Cargo.toml
+    sed -i "s/version = \"$1\"/version = \"$2\"/g" pwm-db/Cargo.toml
+}
+
+update 0.3.5 0.3.6
